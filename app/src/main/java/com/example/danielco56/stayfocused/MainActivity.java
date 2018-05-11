cf92eb87f6bcc53805a77c843f2e2de948588115
danielco56 <danielco56@gmail.com>
2018-05-12 00:06:52 +0300

    fdafa

--- app/src/main/java/com/example/danielco56/stayfocused/MainActivity.java
+++ app/src/main/java/com/example/danielco56/stayfocused/MainActivity.java
@@ -1,17 +1,26 @@
 package com.example.danielco56.stayfocused;
 
+import android.app.ActivityManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Chronometer;
+
+import java.util.Calendar;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button stopButton;
+    private Chronometer cronometru;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        stopButton = (Button) findViewById(R.id.stopButton);
+        cronometru= (Chronometer) findViewById (R.id.cron);
 
-        System.out.println("test");
-
-        System.out.println("fzvzcvd");
     }
 }
