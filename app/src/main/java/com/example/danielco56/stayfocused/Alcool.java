6d74236f9bf3141c01b76179d834a1edd576ed86
danielco56 <danielco56@gmail.com>
2018-05-12 16:05:45 +0300

    New Commit

---
+++ app/src/main/java/com/example/danielco56/stayfocused/Alcool.java
@@ -0,0 +1,19 @@
+package com.example.danielco56.stayfocused;
+
+public class Alcool {
+    int cantitate;
+    int concentratie;
+
+    public Alcool(int cantitate, int concentratie) {
+        this.cantitate = cantitate;
+        this.concentratie = concentratie;
+    }
+
+    public int getCantitate() {
+        return cantitate;
+    }
+
+    public int getConcentratie() {
+        return concentratie;
+    }
+}
