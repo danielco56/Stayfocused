4ffe7f40650b4b170c2d64072241f2bc177c2196
Norbi <lukacs.norbert16@gmail.com>
2018-05-12 16:57:02 +0300

    h

--- app/src/main/java/com/example/danielco56/stayfocused/FirstPage.java
+++ app/src/main/java/com/example/danielco56/stayfocused/FirstPage.java
@@ -3,61 +3,52 @@ package com.example.danielco56.stayfocused;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class FirstPage extends AppCompatActivity {
 
+    private String varsta;
+    private String inaltime;
+    private String greutate;
+
     public static int VVVarsta;
     public static int IIInaltime;
     public static int GGGreutate;
+
+    EditText editText1;
+    EditText editText2;
+    EditText editText3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
 
-        EditText editText1 = (EditText)findViewById(R.id.Varsta);
-
-        EditText editText2 = (EditText)findViewById(R.id.Inaltime);
-
-        EditText editText3 = (EditText)findViewById(R.id.Greutate);
 
 
         Button next = (Button)findViewById(R.id.next);
 
+        editText1 = findViewById(R.id.Vvvarsta);
 
-       // VVVarsta = editText1.getText().toString();
-       // IIInaltime = editText2.getText().toString();
-       // GGGreutate = editText3.getText().toString();
+        editText2 = findViewById(R.id.Iiinaltime);
+        editText3 = findViewById(R.id.Gggreutate);
 
-
-        try {
-            VVVarsta = Integer.parseInt(editText1.getText().toString());
-        } catch(NumberFormatException nfe) {
-            System.out.println("Could not parse " + nfe);
-        }
-
-        try {
-            GGGreutate = Integer.parseInt(editText2.getText().toString());
-        } catch(NumberFormatException nfe) {
-            System.out.println("Could not parse " + nfe);
-        }
-
-        try {
-            IIInaltime = Integer.parseInt(editText2.getText().toString());
-        } catch(NumberFormatException nfe) {
-            System.out.println("Could not parse " + nfe);
-        }
 
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                greutate = String.valueOf(editText3.getText().toString());
+                GGGreutate = Integer.parseInt(greutate);
                 Intent first = new Intent(FirstPage.this, MainActivity.class);
                 startActivity(first);
             }
         });
     }
+
 }
